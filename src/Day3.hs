f40def97669779696275bf518e603c43c50751ed
Antti Lehtonen <anttileh@gmail.com>
2024-12-07 11:42:37 +0200

    Day3 after.

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,5 +1,6 @@
 module Day3 where
 
+import Data.List (isPrefixOf)
 import Text.Regex.PCRE
 
 toInt :: [String] -> (Int, Int)
@@ -16,3 +17,25 @@ result3 = do
   content <- readFile "src/input/3.txt"
   let results = extractAndSumMul content
   putStrLn $ "Total sum of multiplications: " ++ show results
+
+removeBetweenDontAndDo :: String -> String
+removeBetweenDontAndDo content = go content
+  where
+    go remaining
+      | null remaining = "" -- Base case: if the remaining string is empty, return an empty string
+      | "don't()" `isPrefixOf` remaining =
+          let afterDont = drop (length "don't()") remaining
+           in go (dropWhile (/= 'd') afterDont) -- Skip until the next 'do()'
+      | "do()" `isPrefixOf` remaining =
+          let afterDo = drop (length "do()") remaining
+           in go afterDo -- Continue after 'do()'
+      | otherwise =
+          let (str, afterAny) = break (== 'd') remaining -- Capture until the next delimiter
+           in str ++ go afterAny -- Accumulate the string and continue
+
+result3Part2 :: IO ()
+result3Part2 = do
+  content <- readFile "src/input/3.txt"
+  let contentWhereDontIsRemoved = removeBetweenDontAndDo content
+  let results = extractAndSumMul contentWhereDontIsRemoved
+  putStrLn $ "Total sum of multiplications part 2: " ++ show results
--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Day1 (result1)
 import Day1Part2 (result1Part2)
 import Day2 (result2, result2Part2)
-import Day3 (result3)
+import Day3 (result3, result3Part2)
 
 main :: IO ()
 main = do
@@ -17,3 +17,5 @@ main = do
   result2Part2
   putStrLn "Day 3"
   result3
+  putStrLn "Day 3 Part 2"
+  result3Part2
